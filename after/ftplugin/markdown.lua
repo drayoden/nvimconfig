cdbe26c0dc42c671050a4948de8a4a3420b5b164
daryn <me@daryn.dev>
2026-03-06 16:50:22 -0800

    tabspaces for markdown, word wrap

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,11 @@
+vim.o.tabstop = 2       -- A tab character looks like 2 spaces
+vim.o.expandtab = true  -- Use spaces instead of tabs
+vim.o.shiftwidth = 2    -- Size of an indent
+vim.o.softtabstop = 2   -- Number of spaces tab clicks in insert mode
+vim.o.autoindent = true -- Copy indent from current line when starting a new line
+
+
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true -- Indent wrapped lines
+vim.o.textwidth = 80
