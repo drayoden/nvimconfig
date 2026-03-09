fb4c02113c3ff4e8b1139f667a4667d6b5d71378
daryn <me@daryn.dev>
2026-03-09 12:57:43 -0700

    update nvim, key bindings for writing

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,10 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- My bindings
+map("n", "<Leader>mp", ":MarkdownPreview<CR>", { desc = "Start markdown-preview"})
+map("n", "<Leader>ms", ":MarkdownPreviewStop<CR>", { desc = "Stop markdown-preview"})
+map("n", "<Leader>wt", ":set wrap!<CR>", { desc = "Toggle wordwrap"})
+map("n", "<Leader>s", ":set spell! spelllang=en_us<CR>", { desc = "Toggle spell check"})
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
